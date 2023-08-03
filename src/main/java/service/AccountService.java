f2d56d3664449d769f46ba7492c0c40e346702a4
tmiracle527 <132632770+tmiracle527@users.noreply.github.com>
2023-08-03 09:52:19 +0700

    update role AService_Minh_03.08

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -2,7 +2,9 @@ package service;
 
 import fillter.ConnectToMySQL;
 import model.Account;
+import model.Role;
 import service.IService.IProductService;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
@@ -11,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 public class AccountService {
     List<Account> accounts = new ArrayList<>();
     Connection connection = ConnectToMySQL.getConnection();
@@ -42,19 +45,19 @@ public class AccountService {
 
     public List<Account> findAll() {
         List<Account> userList = new ArrayList<>();
-        String sql = "select  * from account;";
+        String sql = "select  * from account join role on account.role_id = role.id;";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
-                String user = resultSet.getString("user");
+                String userName = resultSet.getString("user");
                 String pass = resultSet.getString("pass");
-                int role_Id = resultSet.getInt("role_Id");
+                String roleName = resultSet.getString("name");
                 String address = resultSet.getString("address");
-                int tel = resultSet.getInt("tel");
-
-                Account account = new account(id, user, pass, role_Id, name, age, address, image);
+                String tell = resultSet.getString("tel");
+                Role role=new Role(roleName);
+                Account account = new Account(id, userName, pass, address, tell, role);
                 userList.add(account);
             }
         } catch (SQLException e) {
@@ -62,6 +65,7 @@ public class AccountService {
         }
         return userList;
     }
+
     public boolean checkUser(String userName, String password) {
         accounts = findAll();
         for (int i = 0; i < accounts.size(); i++) {
